504633e96e376c910c37758782b57ab69fa9853d
Lucas Miller <millerlu19@hanover.edu>
2018-05-23 13:59:48 -0400

    Erased ResultsActivity and created Correct Activity

--- app/src/main/java/edu/hanover/cs323_lucasnickproject/CorrectActivity.java
+++ app/src/main/java/edu/hanover/cs323_lucasnickproject/CorrectActivity.java
@@ -3,11 +3,11 @@ package edu.hanover.cs323_lucasnickproject;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class ResultsActivity extends Activity {
+public class CorrectActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_results);
+        setContentView(R.layout.activity_correct);
     }
 }
