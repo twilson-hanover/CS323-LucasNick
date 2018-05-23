fbf1ec2fe574103949bd1074ecb531db7c84331d
Lucas Miller <millerlu19@hanover.edu>
2018-05-23 13:52:59 -0400

    Created ResultsActivity

--- app/src/main/java/edu/hanover/cs323_lucasnickproject/ResultsActivity.java
+++ app/src/main/java/edu/hanover/cs323_lucasnickproject/ResultsActivity.java
@@ -2,15 +2,12 @@ package edu.hanover.cs323_lucasnickproject;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.EditText;
 
-public class GuessActivity extends Activity {
+public class ResultsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guess);
+        setContentView(R.layout.activity_results);
     }
 }
